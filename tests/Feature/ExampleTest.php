306fbba3b50aadb6cdf059234ee28c3518076c19
ashish dahal <59206630+ashish8216@users.noreply.github.com>
2025-11-14 14:29:53 +0545

    Refactor ExampleTest to use RefreshDatabase trait

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,22 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get(route('login'));
+namespace Tests\Feature;
 
-    $response->assertStatus(200);
-});
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+    }
+}
